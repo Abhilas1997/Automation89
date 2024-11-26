af9660ca787e42b382aa97b739e361b7c8490d4c
Abhilas1997 <mohapatraabhilas24@gmail.com>
2024-11-26 23:51:40 +0530

    h

--- src/JavaArray/DuplicateNumber.java
+++ src/JavaArray/DuplicateNumber.java
@@ -20,23 +20,4 @@ public class DuplicateNumber {
 	}
 
 }
-public class DuplicateNumber {
 
-	public static void main(String[] args) 
-	{
-	int [] a= {22,12,3343,33,22,35};
-	for(int i=0;i<a.length; i++)
-	{
-		for(int j=0;j<a.length; j++)
-		{
-			if(a[i]==a[j] && i!=j)
-			{
-				System.out.print(a[i]);
-			}
-		break;
-		}
-	}
-
-	}
-
-}
